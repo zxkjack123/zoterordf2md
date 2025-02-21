7184a2f38d467aa94655497df3cfc709e5cb7b00
zxkjack123 <zxkjack123@163.com>
2025-02-21 10:22:47 +0800

    put md in one folder

--- zoterordf2md.py
+++ zoterordf2md.py
@@ -1,5 +1,4 @@
 import argparse
-import glob
 import re
 import subprocess
 import os
@@ -16,18 +15,18 @@ def extract_pdf_paths(rdf_file):
     return pdf_paths
 
 
-def convert_pdf_to_md(pdf_path, output_folder, method="auto"):
+def convert_pdf_to_md(pdf_path, tmp_folder, method="auto"):
     """Convert a PDF file to a Markdown file using the magic-pdf application."""
     filename = os.path.basename(pdf_path)
     base_name = os.path.splitext(filename)[0]
-    md_folder = os.path.join(output_folder, base_name, method)
+    md_folder = os.path.join(tmp_folder, base_name, method)
     md_name = os.path.join(md_folder, base_name + ".md")
 
     os.makedirs(md_folder, exist_ok=True)
 
     try:
         subprocess.run(['magic-pdf', '-p', pdf_path,
-                        '-o', output_folder, '-m', method], check=True)
+                        '-o', tmp_folder, '-m', method, '-e', "0"], check=True)
         print(f"Converted {pdf_path} to {md_name}")
     except subprocess.CalledProcessError as e:
         print(f"Failed to convert {pdf_path} to Markdown. Error: {e}")
@@ -39,12 +38,17 @@ def main():
         description="Extract PDF paths from an RDF file and convert them to Markdown.")
     parser.add_argument("rdf_file_path", type=str,
                         help="Path to the RDF file to process.")
+    parser.add_argument("--method", type=str, default="auto",
+                        help="Method to use for conversion.")
+    parser.add_argument("--tmp_folder", type=str, default="tmp",
+                        help="Folder to save temporary files.")
     parser.add_argument("--output_folder", type=str, default="results",
                         help="Folder to save the Markdown files.")
     args = parser.parse_args()
 
     # Create the output folder if it doesn't exist
     os.makedirs(args.output_folder, exist_ok=True)
+    os.makedirs(args.tmp_folder, exist_ok=True)
 
     # Extract PDF paths
     pdf_paths = extract_pdf_paths(args.rdf_file_path)
@@ -52,20 +56,17 @@ def main():
 
     # Convert each PDF to Markdown
     for pdf_path in pdf_paths:
-        convert_pdf_to_md(pdf_path, args.output_folder)
-
-    # cleanup tempary files
-    post_fix_clean = ["_content_list.json", "_layout.pdf",
-                      "_middle.json", "_model.json", "_origin.pdf", "_spans.pdf"]
-    for pdf_path in pdf_paths:
+        convert_pdf_to_md(pdf_path, args.tmp_folder, args.method)
         filename = os.path.basename(pdf_path)
         base_name = os.path.splitext(filename)[0]
-        for pf in post_fix_clean:
-            temp_file = os.path.join(
-                args.output_folder, base_name, "auto", f"{base_name}{pf}")
-            if os.path.exists(temp_file):
-                print(f"Removing {temp_file}")
-                os.remove(temp_file)
+        md_file = os.path.join(args.tmp_folder, base_name,
+                               args.method, f"{base_name}.md")
+        if os.path.exists(md_file):
+            print(f"Moving {md_file} to {args.output_folder}")
+            os.rename(md_file, os.path.join(
+                args.output_folder, f"{base_name}.md"))
+        else:
+            print(f"Markdown file {md_file} not found. Skipping.")
 
 
 if __name__ == "__main__":
